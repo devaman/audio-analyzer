70bc5d408e217160909d2fa7f01217cc6a151c0b
BootLegAidan <30330512+BootLegAidan@users.noreply.github.com>
2023-01-06 21:41:39 -0600

    Updated to match main branch of the original

--- script.js
+++ script.js
@@ -1,57 +1,129 @@
+  
   // TUNING PERFORMANCE
 var audio1;
 var worker;
 var canvas = document.getElementById("canvas").transferControlToOffscreen();
-var audioEl = document.getElementById('audio')
+var audioEl = document.getElementById('audio') 
 var container = document.getElementById('container')
-// canvas.width = 750;
-// canvas.height = 500;
+var local_stream= null
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+var defaultState= {
+    height: 256,
+    width: 256,
+    color: '#000',
+    displayType: 0,
+    bufferLength: 128,
+    fftSize: 2**14,
+    beatDetection: false
+}
+if (!navigator.getUserMedia)
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+              navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-document.getElementById('upload_audio').addEventListener('click', function(e) {
-    if (!navigator.getUserMedia)
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                  navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    if (navigator.getUserMedia){
-
-        navigator.getUserMedia({audio:true}, async function(stream){
-            url = stream
+var config= {...defaultState}
+function upload_audio(e, file=false) {
+    console.log(file)
+    if(file) {
+        var file = e.target.files[0]
+        var reader = new FileReader();
+        if(local_stream) local_stream.getAudioTracks()[0].enabled = false;
+        reader.onload = async function(evt) {
+            url = evt.target.result;
             const audioCtx = new (window.AudioContext || window.webkitAudioContext)(); // for safari browser // I need to explain the browser restrictions & CORS issues here
+            // let origblob = new Blob(, { type: file.type });// The blob gives us a URL to the video file:
+            // var arrayBuffer = await new Response(origblob).arrayBuffer();
+            let audioBuffer = await audioCtx.decodeAudioData(url);
+            let audioBlob = convertAudioBufferToBlob(audioBuffer)// The blob gives us a URL to the video file:
+            url = window.URL.createObjectURL(audioBlob);
+            audioEl.pause()
+            audioEl.src = url
+      
             if(!worker){
                 worker = new Worker(new URL("./worker.js", window.location));
                 worker.postMessage({ canvas }, [canvas]);
             }
-
+            
             let analyser = null;
-            const filter = audioCtx.createBiquadFilter();// creates an filter node from the audio source
+            let filter = audioCtx.createBiquadFilter();// creates an filter node from the audio source
             filter.type = 'highshelf'
             filter.gain.value = 10
             filter.frequency.value = 400;
-            source = audioCtx.createMediaStreamSource(url)
-
+    
+            // var gainNode = audioCtx.createGain()
+            let lowpass = audioCtx.createBiquadFilter()
+            let highpass = audioCtx.createBiquadFilter()
+    
+            let source = audioCtx.createMediaElementSource(audioEl)
+            source.connect(lowpass)
+            lowpass.connect(highpass)
+            source.connect(audioCtx.destination)
+            
+            lowpass.type = "lowpass"
+            lowpass.frequency.value = 200
+            lowpass.gain.value = -1
+            highpass.type = "highpass"
+            highpass.frequency.value = 10
+            highpass.gain.value = -1
             analyser = audioCtx.createAnalyser();
-            source.connect(analyser)
-
-            analyser.fftSize = 2**12; // controls the size of the FFT. The FFT is a fast fourier transform. Basically the number of sound samples. Will be used to draw bars in the canvas
-
-            const bufferLength = 256;
+            highpass.connect(analyser)
+           
+            analyser.fftSize = defaultState.fftSize // controls the size of the FFT. The FFT is a fast fourier transform. Basically the number of sound samples. Will be used to draw bars in the canvas
+    
+            // const bufferLength = analyser.frequencyBinCount; // the number of data values that dictate the number of bars in the canvas. Always exactly one half of the fft size
+            const bufferLength = defaultState.bufferLength;
             const dataArray = new Uint8Array(bufferLength); // coverting to unsigned 8-bit integer array format because that's the format we need
-
+        
             function animate() {
             analyser.getByteFrequencyData(dataArray); // copies the frequency data into the dataArray in place. Each item contains a number between 0 and 255
-            worker.postMessage({ bufferLength, dataArray }, {});
+            worker.postMessage({ bufferLength, dataArray, config }, {});
             requestAnimationFrame(animate); // calls the animate function again. This method is built in
             }
-
+        
             animate();
-        },function(e) {
-            alert('Error capturing audio.');
-          })
+           
+        };
+        reader.readAsArrayBuffer(file);
     }
-  });
-
+    else {
+       
+        if (navigator.getUserMedia){
+            // local_stream.getAudioTracks()[0].enabled = true;
+            navigator.getUserMedia({audio:true}, async function(stream){
+                local_stream = stream
+                url = stream
+                const audioCtx = new (window.AudioContext || window.webkitAudioContext)(); // for safari browser // I need to explain the browser restrictions & CORS issues here
+                if(!worker){
+                    worker = new Worker(new URL("./worker.js", window.location));
+                    worker.postMessage({ canvas }, [canvas]);
+                }
+                audioEl.src= ''
+                let analyser = null;
+                let source = audioCtx.createMediaStreamSource(url)
+    
+                analyser = audioCtx.createAnalyser();
+                source.connect(analyser)
+                
+                analyser.fftSize = defaultState.fftSize // controls the size of the FFT. The FFT is a fast fourier transform. Basically the number of sound samples. Will be used to draw bars in the canvas
+    
+                const bufferLength = defaultState.bufferLength;
+                const dataArray = new Uint8Array(bufferLength); // coverting to unsigned 8-bit integer array format because that's the format we need
+    
+                function animate() {
+                analyser.getByteFrequencyData(dataArray); // copies the frequency data into the dataArray in place. Each item contains a number between 0 and 255
+                worker.postMessage({ bufferLength, dataArray, config }, {});
+                requestAnimationFrame(animate); // calls the animate function again. This method is built in
+                }
+    
+                animate();
+            },function(e) {
+                alert('Error capturing audio.');
+              })
+        }
+    }
+    
+}
+  
 /*basic*/
 document.getElementById('upload_bg').addEventListener('change', function(e) {
     var file = this.files[0]
@@ -59,12 +131,12 @@ document.getElementById('upload_bg').addEventListener('change', function(e) {
     reader.onload = function(evt) {
         url = evt.target.result;
         if(file.type.startsWith('image')){
-            container.style = `background-image: url(${url}); background-repeat:no-repeat;background-size:contain;background-position:center`
+            container.style = `background-image: url(${url}); background-repeat:no-repeat;background-size:contain;background-position:center;`
         } else {
             let videoBlob = new Blob([new Uint8Array(url)], { type: file.type });// The blob gives us a URL to the video file:
             url = window.URL.createObjectURL(videoBlob);
             const videoElem = document.createElement('video')
-            var sourceMP4 = document.createElement("source");
+            var sourceMP4 = document.createElement("source"); 
             sourceMP4.type = file.type;
             sourceMP4.src = url;
             videoElem.appendChild(sourceMP4);
@@ -91,29 +163,24 @@ document.getElementById('upload_logo').addEventListener('change', function(e) {
     var reader = new FileReader();
     reader.onload = function(evt) {
         url = evt.target.result;
-        // const logo_img = document.createElement('img')
-        const logo_img = document.querySelector('img.logo_img')
+        const logo_img = document.createElement('img')
         logo_img.src = url;
-        // logo_img.width=256
-        // logo_img.height=256
-        // logo_img.classList.add('logo_img')
-        // logo_img.style = 'position:absolute;'
-        // let logoExists = container.querySelector('.logo_img')
-        // if(logoExists){
-        //     container.removeChild(logoExists)
-        // }
-        // container.appendChild(logo_img)
+        logo_img.width=256
+        logo_img.height=256
+        logo_img.classList.add('logo_img')
+        logo_img.style = 'position:absolute;'
+        let logoExists = container.querySelector('.logo_img')
+        if(logoExists){
+            container.removeChild(logoExists)
+        }
+        container.appendChild(logo_img)
     }
     reader.readAsDataURL(file);
 
 })
 document.getElementById('stroke_color').addEventListener('change', function(e) {
     var stroke_color = e.target.value
-    worker.postMessage({ stroke_color }, {});
-})
-document.getElementById('display_num').addEventListener('change', function(e) {
-  var display_num = e.target.value
-  worker.postMessage({ display_num }, {})
+    config.color = stroke_color
 })
 document.addEventListener("keydown", (e) => {
     if (e.key === " ") {
@@ -174,7 +241,7 @@ function toggleFullScreen(e) {
     e.stopPropagation();
     el = document.body; // Make the body go full screen.
     var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) ||  (document.mozFullScreen || document.webkitIsFullScreen);
-
+    
     let img = document.getElementById('fullscreen').querySelector('img')
     if (isInFullScreen) {
         img.src = '/img/fullscreen.png'
@@ -184,6 +251,34 @@ function toggleFullScreen(e) {
         requestFullScreen(el);
     }
 }
+function displayChange(event){
+    let val = event.target.value
+    defaultState.displayType = parseInt(val)
+    config.displayType = parseInt(val)
+}
+function radiusChange(event){
+    let val = event.target.value
+    defaultState.height = val*2
+    defaultState.width = val*2
+    config.width = defaultState.width>window.innerWidth ?window.innerWidth: defaultState.width
+    config.height =  defaultState.height>window.innerHeight ?window.innerHeight: defaultState.height
+}
+function setFFTSize(event){
+    let val = event.target.value
+    defaultState.fftSize = parseInt(val)
+    config.fftSize = parseInt(val)
+}
+function setBufferLength(event){
+    let val = event.target.value
+    defaultState.bufferLength = parseInt(val)
+    config.bufferLength = parseInt(val)
+}
+function setBeatDetection(event){
+    let val = event.target.value
+    defaultState.beatDetection = Boolean(val)
+    config.beatDetection = Boolean(val)
+}
+
 //utility
 function convertAudioBufferToBlob(audioBuffer) {
 // Float32Array samples
@@ -192,8 +287,8 @@ function convertAudioBufferToBlob(audioBuffer) {
     // interleaved
     const interleaved = new Float32Array(left.length + right.length)
     for (let src=0, dst=0; src < left.length; src++, dst+=2) {
-      interleaved[dst] =   left[src]
-      interleaved[dst+1] = right[src]
+    interleaved[dst] =   left[src]
+    interleaved[dst+1] = right[src]
     }
 
     // get WAV file bytes and audio params of your audio source
@@ -211,17 +306,17 @@ function convertAudioBufferToBlob(audioBuffer) {
 function getWavBytes(buffer, options) {
     const type = options.isFloat ? Float32Array : Uint16Array
     const numFrames = buffer.byteLength / type.BYTES_PER_ELEMENT
-
+  
     const headerBytes = getWavHeader(Object.assign({}, options, { numFrames }))
     const wavBytes = new Uint8Array(headerBytes.length + buffer.byteLength);
-
+  
     // prepend header, then add pcmBytes
     wavBytes.set(headerBytes, 0)
     wavBytes.set(new Uint8Array(buffer), headerBytes.length)
-
+  
     return wavBytes
   }
-
+  
   // adapted from https://gist.github.com/also/900023
   // returns Uint8Array of WAV header bytes
   function getWavHeader(options) {
@@ -230,33 +325,33 @@ function getWavBytes(buffer, options) {
     const sampleRate =     options.sampleRate || 44100
     const bytesPerSample = options.isFloat? 4 : 2
     const format =         options.isFloat? 3 : 1
-
+  
     const blockAlign = numChannels * bytesPerSample
     const byteRate = sampleRate * blockAlign
     const dataSize = numFrames * blockAlign
-
+  
     const buffer = new ArrayBuffer(44)
     const dv = new DataView(buffer)
-
+  
     let p = 0
-
+  
     function writeString(s) {
       for (let i = 0; i < s.length; i++) {
         dv.setUint8(p + i, s.charCodeAt(i))
       }
       p += s.length
     }
-
+  
     function writeUint32(d) {
       dv.setUint32(p, d, true)
       p += 4
     }
-
+  
     function writeUint16(d) {
       dv.setUint16(p, d, true)
       p += 2
     }
-
+  
     writeString('RIFF')              // ChunkID
     writeUint32(dataSize + 36)       // ChunkSize
     writeString('WAVE')              // Format
@@ -270,15 +365,15 @@ function getWavBytes(buffer, options) {
     writeUint16(bytesPerSample * 8)  // BitsPerSample
     writeString('data')              // Subchunk2ID
     writeUint32(dataSize)            // Subchunk2Size
-
+  
     return new Uint8Array(buffer)
   }
-
-window.addEventListener('resize', () => {
-  document.querySelector('img.logo_img').width = Math.min(window.innerWidth,window.innerHeight) / 1.5
-  document.querySelector('img.logo_img').height = Math.min(window.innerWidth,window.innerHeight) / 1.5
-  if (worker) {
-    let resize_canvas = [window.innerWidth, window.innerHeight]
-    worker.postMessage({ resize_canvas })
-  }
-})
+  window.addEventListener('resize', () => {
+    if (worker) {
+        console.log('resize')
+        config.width = defaultState.width>window.innerWidth ?window.innerWidth: defaultState.width
+        config.height =  defaultState.height>window.innerHeight ?window.innerHeight: defaultState.height
+        let resize_canvas = [window.innerWidth, window.innerHeight]
+        worker.postMessage({ resize_canvas })
+    }
+  })
--- worker.js
+++ worker.js
@@ -1,215 +1,134 @@
-let displayType = 12 // Can be any integer from 0 - 3
-const background = 'rgba(255,255,255,0.25)'
-
 let canvas = null;
-let color="#222"
-OffscreenCanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
-    if (w < 2 * r) r = w / 2;
-    if (h < 2 * r) r = h / 2;
-    this.beginPath();
-    this.moveTo(x+r, y);
-    this.arcTo(x+w, y,   x+w, y+h, r);
-    this.arcTo(x+w, y+h, x,   y+h, 0);
-    this.arcTo(x,   y+h, x,   y,   0);
-    this.arcTo(x,   y,   x+w, y,   r);
-    this.closePath();
-    return this;
+// const displayType = 0 // Can be any integer from 0 - 3
+// let color="#222"
+const normalize = (val, threshold=200) => ((val > threshold) ? val - threshold : 0);
+const drawVisualizer = ({ bufferLength, dataArray, config }) => {
+  // let radius = 128
+  let radius = Math.min(config.width,config.height) / 3
+  // const barWidth = canvas.width / bufferLength;
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // clears the canvas
+  let threshold = 0
+  if(config.beatDetection) {
+    let max = Math.max(...dataArray.slice(0,bufferLength))
+    let min = Math.min(...dataArray.slice(0,bufferLength))
+    threshold = min + (max - min) * 0.68;
   }
-const avg = (arr) => {
-  let sum = 0;
-  for (let i of arr) {
-    sum += i
+  // ctx.translate(250, 250)
+  // ctx.translate(canvas.width / 2, canvas.height / 2)
+  for (var i = 0; i < bufferLength; i++) {
+    // const height =normalize(dataArray[i],100,0)
+    const height = config.beatDetection ? normalize(dataArray[i], threshold):(dataArray[i] *0.4)
+      // -i>0?(dataArray[i] *0.4)-i:(dataArray[i] *0.4)
+    drawLine(
+      {
+        i,
+        bufferLength,
+        height,
+        radius,
+        config
+      },
+      ctx
+    );
   }
-  return sum / arr.length
-}
-const normalize = (val, max, min) => ((val - min) / (max - min)*10);
-  const drawVisualizer = ({ bufferLength, dataArray }) => {
-    let mean = avg(dataArray)
-    let max = Math.max(...dataArray)
-    let min = Math.min(...dataArray)
-    // let radius = 128
-    let radius = Math.min(canvas.width,canvas.height) / 3
-    // const barWidth = canvas.width / bufferLength;
-    const ctx = canvas.getContext("2d");
-    // ctx.clearRect(0, 0, canvas.width, canvas.height); // clears the canvas
-    ctx.fillStyle = background
-    ctx.resetTransform()
-    ctx.fillRect(0,0,canvas.width,canvas.height)
-    // ctx.translate(250, 250)
-    // ctx.translate(canvas.width / 2, canvas.height / 2)
-    for (var i = 0; i < bufferLength; i++) {
-        // const height =normalize(dataArray[i],100,0) * 10
-        const height = (dataArray[i] *0.4)
-        // -i>0?(dataArray[i] *0.4)-i:(dataArray[i] *0.4)
+};
+const drawLine = (opts, ctx) => {
+  const { i, radius, bufferLength, height, config } = opts;
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+  const lineWidth = 2 * Math.PI * radius / bufferLength;
+  const rads = (Math.PI * 2) / bufferLength;
 
-        drawLine(
-          {
-            i,
-            bufferLength,
-            height,
-            radius,
-            mean,
-            max,
-            min
-          },
-          ctx
-        );
+  const x = centerX + Math.cos(rads * i) * (radius);
+  const y = centerY + Math.sin(rads * i) * (radius);
+  const endX = centerX + Math.cos(rads * i) * (radius + height);
+  const endY = centerY + Math.sin(rads * i) * (radius + height);
+  let width = canvas.width / bufferLength;
+  ctx.strokeStyle = config.color;
+  ctx.fillStyle = config.color
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = "round";
+  switch (config.displayType) {
+    case 1:
+      if (i == 0) {
+        ctx.beginPath()
+        ctx.moveTo(endX,endY)
       }
-  };
-  const drawLine = (opts, ctx) => {
-    const { i, radius, bufferLength, height, mean, max, min } = opts;
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-    const lineWidth = 2 * Math.PI * radius / bufferLength;
-    const rads = (Math.PI * 2) / bufferLength;
-
-    const x = centerX + Math.cos(rads * i) * (radius);
-    const y = centerY + Math.sin(rads * i) * (radius);
-    const endX = centerX + Math.cos(rads * i) * (radius + height);
-    const endY = centerY + Math.sin(rads * i) * (radius + height);
-    // console.log(x,y,endX,endY)
-    // draw the bar
-    let width = canvas.width / bufferLength;
-    ctx.strokeStyle = color;
-    ctx.fillStyle = color
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = "round";
-    switch (displayType) {
-      case 1:
-        if (i == 0) {
-          ctx.beginPath()
-          ctx.moveTo(endX,endY)
-        }
-        ctx.lineTo(endX,endY)
-        if (i == bufferLength - 1) {
-          ctx.fill()
-        }
-      break; case 2:
-        ctx.fillRect(i * width, 0, width, height)
-      break; case 3:
-        // let width = canvas.width / bufferLength;
-        if (i == 0) {
-          ctx.beginPath()
-          ctx.moveTo(0, 0)
-        }
-        ctx.lineTo(i * width, height)
-        if (i == bufferLength - 1) {
-          ctx.lineTo(canvas.width,0)
-          ctx.fill()
-        }
-      break; case 4:
-        ctx.fillRect(i * width, centerY - height, width, height * 2)
-      break; case 5:
-        ctx.fillStyle = `hsl(${height},70%,60%)`
-        ctx.fillRect(i * width, centerY - height, width, height * 2)
-        break; case 6:
-          ctx.beginPath()
-          ctx.arc(x,y,height,0,10)
-          ctx.fill()
-      break; case 7:
-        ctx.beginPath()
-        ctx.arc(endX,endY,5,0,10)
+      ctx.lineTo(endX,endY)
+      if (i == bufferLength - 1) {
         ctx.fill()
-      break; case 8:
-        for (let j = 1; j < 6; j++) {
-          ctx.beginPath()
-          ctx.arc(
-            ((endX - centerX) * ((1/5) * j)) + centerX,
-            ((endY - centerY) * ((1 / 5) * j)) + centerY,
-            5,
-            0,
-            10
-          )
-          ctx.fill()
-        }
-      break; case 9:
+      }
+    break; case 2:
+      // let width = canvas.width / bufferLength;
+      ctx.fillRect(i * width, 0, width, height)
+    break; case 3:
+      // let width = canvas.width / bufferLength;
+      if (i == 0) {
         ctx.beginPath()
-        ctx.arc(i * width,centerY,height,0,10)
+        ctx.moveTo(0, 0)
+      }
+      ctx.lineTo(i * width, height)
+      if (i == bufferLength - 1) {
+        ctx.lineTo(canvas.width,0)
         ctx.fill()
-      break; case 10:
+      }
+    break; case 4:
+      ctx.fillRect(i * width, centerY - height, width, height * 2)
+    break; case 5:
+      let color = ctx.fillStyle.slice(1)
+      ctx.fillStyle = `rgba(${parseInt(color.slice(0,2),16)},${parseInt(color.slice(2,4),16)},${parseInt(color.slice(4,6),16)},${height / 64})`
+      ctx.fillRect(i * width, 0, width, canvas.height)
+    break; case 6:
+      ctx.beginPath()
+      ctx.arc((Math.sin(rads * i) * radius * 1.5) + centerX,(Math.cos(rads * i) * radius * 1.5)+centerY,height / 2,0,Math.PI * 2)
+      ctx.fill()
+    break; case 7:
+      ctx.beginPath()
+      ctx.arc(endX,endY,5,0,10)
+      ctx.fill()
+    break; case 8:
+      for (let j = 1; j < 6; j++) {
         ctx.beginPath()
-        ctx.moveTo(
-          centerX + Math.cos(rads * i) * (radius - height),
-          centerY + Math.sin(rads * i) * (radius - height)
+        ctx.arc(
+          centerX + (Math.sin(rads * i) * (height + radius) * (j / 3)),
+          centerY + (Math.cos(rads * i) * (height + radius) * (j / 3)),
+          5,
+          0,
+          10
         )
-        ctx.lineTo(
-          centerX + Math.cos(rads * i) * (radius + height),
-          centerY + Math.sin(rads * i) * (radius + height)
-        )
-        ctx.stroke()
-      break; case 11:
-        ctx.resetTransform()
-        ctx.translate(centerX, centerY)
-        // ctx.scale(100 / mean, 100 / mean)
-        ctx.rotate(rads * i)
-        // ctx.fillRect(0,0,(radius / bufferLength) * Math.PI * 2,height + radius)
-
-        ctx.fillRect(0,(max * 0.4) + radius,10,10)
-        ctx.fillRect(0,(min * 0.4) + radius,10,10)
-        ctx.fillRect(0,(mean * 0.4) + radius,10,10)
-      break; case 12:
-        ctx.resetTransform()
-        ctx.fillStyle = color
-        ctx.translate(centerX, centerY)
-        ctx.rotate(((Math.PI * 2) / (bufferLength - 1)) * i)
-        if (i == 0) {
-          ctx.beginPath()
-          ctx.moveTo(0,radius-height)
-
-        }
-        // ctx.fillRect(0,radius,(radius / bufferLength) * Math.PI * 2,-(height))
-        ctx.lineTo(0,radius-height)
-        if (i >= bufferLength - 1) {
-          ctx.rotate(-((Math.PI * 2) / (bufferLength - 1)) * i)
-          ctx.lineTo(0,radius * 2)
-          ctx.lineTo(radius* 2,radius * 2)
-          ctx.lineTo(radius * 2,-radius * 2)
-          ctx.lineTo(-radius * 2,-radius * 2)
-          ctx.lineTo(-radius * 2,radius * 2)
-          ctx.lineTo(0,radius * 2)
-          ctx.fill()
-        }
-      break; default:
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.lineTo(endX, endY);
-        ctx.stroke();
+        ctx.fill()
+      }
+    break; case 9:
+      ctx.beginPath()
+      ctx.arc(i * width,centerY,height,0,10)
+      ctx.fill()
+    break; case 10:
+      ctx.beginPath()
+      ctx.moveTo(
+        centerX + Math.cos(rads * i) * (radius - height),
+        centerY + Math.sin(rads * i) * (radius - height)
+      )
+      ctx.lineTo(
+        centerX + Math.cos(rads * i) * (radius + height),
+        centerY + Math.sin(rads * i) * (radius + height)
+      )
+      ctx.stroke()
+    break; default:
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(endX, endY);
+      ctx.stroke();
     }
-
-
-    // ctx.fillRect(i,0,3,height)
-
-    // ctx.beginPath();
-    // ctx.moveTo(x, y);
-    // ctx.arcTo(x+endX, y,   x+endX, y+endY, 2);
-    // ctx.arcTo(x+endX, y+endY, x,   y+endY, 0);
-    // ctx.arcTo(x,   y+endY, x,   y,   0);
-    // ctx.arcTo(x,   y,   x+endX, y,   2);
-    // ctx.stroke()
-    // ctx.roundRect(x,y,endX,endY).fill()
-    // ctx.fillStyle = 'white'
-    // ctx.fillRect(x, y, endX+lineWidth, endY);
 
   };
   onmessage = function (e) {
-    // console.log("Worker: Message received from main script");
-    const { bufferLength, dataArray, stroke_color,logo, canvas: canvasMessage, resize_canvas, display_num } = e.data;
+    console.log("Worker: Message received from main script");
+    const { bufferLength, dataArray, config, canvas: canvasMessage, resize_canvas } = e.data;
     if (canvasMessage) {
       canvas = canvasMessage;
-    } else if(stroke_color){
-        color=stroke_color
-    } else if(logo){
-        const ctx = canvas.getContext("2d");
-        this.createImageBitmap(logo).then(img=>{
-            ctx.drawImage(img,0,0); // Or at whatever offset you like
-        })
-
     } else if (resize_canvas) {
       [canvas.width, canvas.height] = resize_canvas
-    } else if (display_num) {
-      displayType = parseInt(display_num)
-      // console.log(displayType);
-    } else {
-      drawVisualizer({ bufferLength, dataArray });
+    }  else {
+      drawVisualizer({ bufferLength, dataArray, config });
     }
   };
